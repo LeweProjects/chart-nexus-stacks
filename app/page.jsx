3f1508bd616606eb9def3664cb21d09bacde6dae
Marvell Ayon <loewiayon12@gmail.com>
2024-07-19 00:37:40 +0800

    added suspense boundary

--- app/page.jsx
+++ app/page.jsx
@@ -17,33 +17,33 @@ export default function Home() {
 			} duration-150 flex min-h-screen flex-col items-center bg-background justify-between p-24`}
 		>
 			<div className="flex flex-col w-[80%] max-w-[55rem] ">
-				<section className="w-full flex justify-between text-text-color bg-header-bg p-4">
-					<label className="font-semibold flex items-center">
-						DELAYED QUOTES
-					</label>
-					<select
-						name=""
-						id=""
-						value={exchange}
-						onChange={handleChange}
-						className="rounded-md text-black"
-					>
-						<option value="0">SGX</option>
-						<option value="2">Bursa</option>
-						<option value="3">Nasdaq</option>
-					</select>
-					<div className="flex flex-col justify-center items-center">
-						<p className="text-sm font-semibold">Dark Mode</p>
-						<DarkModeToggle
-							onChange={() => setTheme(!theme)}
-							checked={theme}
-							size={70}
-						/>
-					</div>
-				</section>
+				<Suspense>
+					<section className="w-full flex justify-between text-text-color bg-header-bg p-4">
+						<label className="font-semibold flex items-center">
+							DELAYED QUOTES
+						</label>
+						<select
+							name=""
+							id=""
+							value={exchange}
+							onChange={handleChange}
+							className="rounded-md text-black"
+						>
+							<option value="0">SGX</option>
+							<option value="2">Bursa</option>
+							<option value="3">Nasdaq</option>
+						</select>
+						<div className="flex flex-col justify-center items-center">
+							<p className="text-sm font-semibold">Dark Mode</p>
+							<DarkModeToggle
+								onChange={() => setTheme(!theme)}
+								checked={theme}
+								size={70}
+							/>
+						</div>
+					</section>
 
-				<section className="grid grid-cols-5 border-b-2 text-text-color bg-header-bg border-filters chartHeader">
-					<Suspense fallback={<div>Loading...</div>}>
+					<section className="grid grid-cols-5 border-b-2 text-text-color bg-header-bg border-filters chartHeader">
 						<Link
 							href={`?market_id=${marketId}&list=0`}
 							className={`${
@@ -52,8 +52,6 @@ export default function Home() {
 						>
 							Top Volume
 						</Link>
-					</Suspense>
-					<Suspense fallback={<div>Loading...</div>}>
 						<Link
 							href={`?market_id=${marketId}&list=1`}
 							className={`${
@@ -62,8 +60,6 @@ export default function Home() {
 						>
 							Top Gainers
 						</Link>
-					</Suspense>
-					<Suspense fallback={<div>Loading...</div>}>
 						<Link
 							href={`?market_id=${marketId}&list=2`}
 							className={`${
@@ -72,8 +68,6 @@ export default function Home() {
 						>
 							Top Losers
 						</Link>
-					</Suspense>
-					<Suspense fallback={<div>Loading...</div>}>
 						<Link
 							href={`?market_id=${marketId}&list=3`}
 							className={`${
@@ -82,8 +76,6 @@ export default function Home() {
 						>
 							Top % Gainer
 						</Link>
-					</Suspense>
-					<Suspense fallback={<div>Loading...</div>}>
 						<Link
 							href={`?market_id=${marketId}&list=4`}
 							className={`${
@@ -92,40 +84,38 @@ export default function Home() {
 						>
 							Top % Losers
 						</Link>
-					</Suspense>
-				</section>
+					</section>
 
-				<section className="grid col-span-5 border-[1px] border-table-bg p-3 bg-table-bg overflow-y-scroll max-h-screen">
-					<table className="chartTable">
-						<tbody>
-							<tr className="sticky -top-3 bg-table-bg text-text-color">
-								<th>
-									<p className="text-left px-3">
-										Stock <br /> Code
-									</p>
-								</th>
-								<th>
-									Last <br /> Vol
-								</th>
-								<th>
-									+/- <br /> %Chng
-								</th>
-								<th>
-									Buy <br /> Buy Vol
-								</th>
-								<th>
-									Sell <br /> Sell Vol
-								</th>
-								<th></th>
-							</tr>
-							<Suspense fallback={<div>Loading...</div>}>
+					<section className="grid col-span-5 border-[1px] border-table-bg p-3 bg-table-bg overflow-y-scroll max-h-screen">
+						<table className="chartTable">
+							<tbody>
+								<tr className="sticky -top-3 bg-table-bg text-text-color">
+									<th>
+										<p className="text-left px-3">
+											Stock <br /> Code
+										</p>
+									</th>
+									<th>
+										Last <br /> Vol
+									</th>
+									<th>
+										+/- <br /> %Chng
+									</th>
+									<th>
+										Buy <br /> Buy Vol
+									</th>
+									<th>
+										Sell <br /> Sell Vol
+									</th>
+									<th></th>
+								</tr>
 								{chart?.map((res, i) => (
 									<ChartComponent key={i} res={res} />
 								))}
-							</Suspense>
-						</tbody>
-					</table>
-				</section>
+							</tbody>
+						</table>
+					</section>
+				</Suspense>
 			</div>
 		</main>
 	);
